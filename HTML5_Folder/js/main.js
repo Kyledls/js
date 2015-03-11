6499959c4819504c0d13facf7ed570c02bce31e7
Kyle <kyle@edwards-mbp-2.hsd1.nj.comcast.net>
2015-03-11 17:49:11 -0400

    HTML5

--- HTML5_Folder/js/main.js
+++ HTML5_Folder/js/main.js
@@ -1,1 +1,10 @@
-
+function showMood(){
+    name = document.getElementById('name').value;
+    name = document.getElementById('mood').value;
+    
+    moodString = name+" is "+mood+" today ";
+    
+    holder = document.getElementById('holder').value;
+    
+    holder.innerHTML = moodString;
+}
